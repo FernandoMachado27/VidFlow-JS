3900c2ad6f5a6f0c3e65a75343192075bdcc9149
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-10-24 15:31:59 -0300

    Async/Await

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const containerVideos = document.querySelector(".videos__container"); // captura lista ul do HTML
 
-const api = fetch("http://localhost:3000/videos") // retorna a promise -> deve estar fulfilled
-.then(res => res.json()) // retorna dados da API em json
-.then((videos) =>
-    videos.forEach((video)=>{ // para cada video...
-        containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                    <div class="descricao-video">
-                        <img class="img-canal" src"${video.imagem}" alt="Logo do Canal">
-                        <h3 class="titulo-video">${video.titulo}</h3>
-                            <p class="titulo-canal">${video.descricao}</p>
-                    </div>   
-            </li>
-        `; // inserir os videos no HTML
-    })
-)
 
-.catch((error) =>{
-    containerVideos.innerHTML = `<p> Houve um erro ao carrecar os vídeos: ${error} </p>`;
-})
+async function buscarEMostrarVideos(){
+    const busca = await fetch("http://localhost:3000/videos") // retorna a promise -> deve estar fulfilled, await aguarda que a busca seja feita para continuar o código
+    const videos = await busca.json(); // retorna dados da API em json
+        videos.forEach((video)=>{ // para cada video...
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                        <div class="descricao-video">
+                            <img class="img-canal" src"${video.imagem}" alt="Logo do Canal">
+                            <h3 class="titulo-video">${video.titulo}</h3>
+                                <p class="titulo-canal">${video.descricao}</p>
+                        </div>   
+                </li>
+            `; // inserir os videos no HTML
+        })
+}
+
+buscarEMostrarVideos();
